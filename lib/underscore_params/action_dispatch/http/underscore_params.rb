e68b9ce2196fac1bc84cb6d9ab3bfc93961c6565
ansonK <ansonkelly@gmail.com>
2014-06-01 18:55:51 +0100

    added some documentation

--- lib/underscore_params/action_dispatch/http/underscore_params.rb
+++ lib/underscore_params/action_dispatch/http/underscore_params.rb
@@ -1,13 +1,29 @@
 module ActionDispatch
   module Http
+
+    #
+    # Overrides the default params method on ActionDispatch::Request to underscore all keys
+    # (request.params is aliased to parameters)
+    #
+    # Should be included into ActionDispatch::Request after the standard Rails modules
+    #
     module UnderscoreParams
 
+      #
+      # Processes parameters by underscoring all hash keys
+      #
       def parameters
         underscore_keys super
       end
 
       private
         
+      #
+      # Underscores all key values in both Hashes and Hashes in arrays.
+      # Other values are not modified
+      #
+      # @param value [Object] The value to be processed
+      #
       def underscore_keys(value)
         case value
           when Array
